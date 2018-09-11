d6b23fd4c983afda9959253372ec236ad3028ae5
Caine Atkinson <rc0033222@student.riversidecollege.ac.uk>
2018-09-11 10:40:26 +0100

    added arrows to the buttons instead of text

--- move picture/Form1.Designer.cs
+++ move picture/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,49 +49,59 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(675, 137);
+            this.button1.Location = new System.Drawing.Point(687, 144);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(47, 23);
+            this.button1.Size = new System.Drawing.Size(24, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Up";
+            this.button1.Text = "↑";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(675, 181);
+            this.button2.Location = new System.Drawing.Point(687, 185);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(47, 23);
+            this.button2.Size = new System.Drawing.Size(24, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Down";
+            this.button2.Text = "↓";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(622, 158);
+            this.button3.Location = new System.Drawing.Point(666, 166);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(47, 23);
+            this.button3.Size = new System.Drawing.Size(24, 23);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Left";
+            this.button3.Text = "←";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(728, 158);
+            this.button4.Location = new System.Drawing.Point(708, 166);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(47, 23);
+            this.button4.Size = new System.Drawing.Size(24, 23);
             this.button4.TabIndex = 4;
-            this.button4.Text = "Right";
+            this.button4.Text = "→";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(675, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Controls";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -100,6 +111,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -110,6 +122,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
