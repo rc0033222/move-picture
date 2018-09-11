ae77ca8ca9ab8e58f4101689c84a2602d12528c9
Caine Atkinson <rc0033222@student.riversidecollege.ac.uk>
2018-09-11 10:56:32 +0100

    added all the comments to my code

--- move picture/Form1.cs
+++ move picture/Form1.cs
@@ -21,40 +21,41 @@ namespace move_picture
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 15);
         }
-
+        // moves the image up
         private void button2_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 15);
         }
-
+        // moves the image dowm
         private void button4_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 15, pictureBox1.Location.Y);
         }
-
+        // moves the image left
         private void button3_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 15, pictureBox1.Location.Y);
         }
-
+        // moves the image right
         private void button5_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 15, pictureBox1.Location.Y - 15);
         }
-
+        // moves the image to the top left
         private void button6_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 15, pictureBox1.Location.Y - 15);
         }
-
+        // moves the image to the top right
         private void button7_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 15, pictureBox1.Location.Y + 15);
         }
-
+        // moves the image to the bottom left
         private void button8_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 15, pictureBox1.Location.Y + 15);
         }
+        // moves the image to the bottom right
     }
 }
